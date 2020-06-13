ab8a88f925c679d97c181927f5d151f8c0ccdff7
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-13 18:28:38 +0100

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+# INPUT : bob
+
+    string = input("enter a string:")
+    rev_string = string[str::-1]
+if string == rev_string:
+    print("It is a palindrome";)
+else:
+   print("it is not palindrome";)
