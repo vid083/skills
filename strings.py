25ceb4e454def48a28745eb10af9db8c3252ffbd
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-14 00:45:44 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,41 @@
+#to get the character at position 1 of a string
+
+a = "Hello, World!"
+print(a[1])
+
+#substring
+
+b = "Hello, World!"
+print(b[2:5])
+
+#remove space from beginning or at the end of a string
+
+a = " Hello, World! "
+print(a.strip())
+
+#return the length of a string
+
+a = "Hello, World!"
+print(len(a))
+
+#to lower
+
+a = "Hello, World!"
+print(a.lower())
+
+#to upper
+
+a = "Hello, World!"
+print(a.upper())
+
+#replace a string with another string
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#split a string into substrings
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+
