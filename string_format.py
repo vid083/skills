259dba0c00a530ede10ede12f8cf7a5b57f4b2b4
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-14 01:08:45 +0100

    Create string_format.py

---
+++ string_format.py
@@ -0,0 +1,8 @@
+#The format() method takes the passed arguments, formats them, and places them in the string where the placeholders {} are:
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
