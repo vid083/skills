9e2951270c09c917c17a43b059bb50229759f688
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-14 01:58:09 +0100

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,10 @@
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+OUTPUT: a and b are equal
+  # The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
