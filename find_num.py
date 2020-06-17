cdaee390cf4ce61e8a5c7dba4440349a64f5809a
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-06-17 02:46:30 +0100

    Create find_num.py

---
+++ find_num.py
@@ -0,0 +1,14 @@
+# Python Program to Check if a Number is Positive, Negative or 0
+
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
+else:
+   print("Negative number")
+
+  # OUTPUT: Enter a number: 2
+            #Positive number
+
